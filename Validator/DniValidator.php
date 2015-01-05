bf966f2d2966e94e5fbccadbfcdce15e5cebc088
Álvaro de la Vega Olmedilla <alvarodlvo@gmail.com>
2015-01-05 18:31:18 +0100

    Added DniValidator

--- Validator/DniValidator.php
+++ Validator/DniValidator.php
@@ -15,7 +15,12 @@ class DniValidator extends ConstraintValidator
 {
     private $dniFormatExpr = '/((^[A-Z]{1}[0-9]{7}[A-Z0-9]{1}$|^[T]{1}[A-Z0-9]{8}$)|^[0-9]{8}[A-Z]{1}$)/';
     private $standardDniExpr = '/(^[0-9]{8}[A-Z]{1}$)/';
-    private $availiableLastChar = 'TRWAGMYFPDXBNJZSQVHLCKE';
+    private $availableLastChar = 'TRWAGMYFPDXBNJZSQVHLCKE';
+
+    /**
+     * @var \Symfony\Component\Validator\Context\ExecutionContextInterface
+     */
+    private $context;
 
     /**
      * Checks if the passed value is valid.
@@ -27,9 +32,7 @@ class DniValidator extends ConstraintValidator
      */
     public function validate($value, Constraint $constraint)
     {
-        $ret = $this->checkDni($value);
-
-        if (!$ret)
+        if (!$this->checkDni($value))
         {
             $this->context->buildViolation($constraint->message)
                 ->addViolation();
@@ -49,7 +52,7 @@ class DniValidator extends ConstraintValidator
     protected function isValidDniLastChar($dni)
     {
         $dniCharacters = $this->splitDni($dni);
-        return ($dniCharacters[8] == substr($this->availiableLastChar, substr($dni, 0, 8) % 23, 1));
+        return ($dniCharacters[8] == substr($this->availableLastChar, substr($dni, 0, 8) % 23, 1));
     }
 
     protected function checkStandardDni($dni)
